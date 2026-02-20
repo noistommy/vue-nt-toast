c020ed423c582bb2cfa07a13dfef9f5438f2e21f
noistommy <hioran@nate.com>
2026-02-20 16:08:10 +0900

    fixed code font-family

--- demo/src/components/GetStarted.vue
+++ demo/src/components/GetStarted.vue
@@ -133,9 +133,6 @@ watch(() => props.themeMode, highlight, {immediate:true})
     padding-top: 1.25rem;
     padding-bottom: 1.25rem;
     background-color: transparent !important;
-    code {
-      font-family: monospacer;
-    }
   }
 }
 
